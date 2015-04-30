477492bcbec2ca2da922edfeb0d3928b743f6bf4
David Abel & David Garber <student@epicodus.com>
2015-04-30 11:00:35 -0700

    returns the label for the number

--- lib/phone.rb
+++ lib/phone.rb
@@ -1,8 +1,9 @@
 class Phone
-  attr_reader(:number, :area_code)
+  attr_reader(:number, :area_code, :label)
 
   define_method(:initialize) do |attributes|
     @number = attributes.fetch(:number)
     @area_code = attributes.fetch(:area_code)
+    @label = attributes.fetch(:label)
     end
   end
--- spec/phone_spec.rb
+++ spec/phone_spec.rb
@@ -3,14 +3,21 @@ require('phone')
 
   describe("#number") do
     it("returns phone number") do
-    test_phone = Phone.new({:number => "555-5555", :area_code => "503"})
+    test_phone = Phone.new({:number => "555-5555", :area_code => "503", :label => "mobile"})
     expect(test_phone.number()).to(eq("555-5555"))
     end
   end
 
   describe('#area_code') do
     it("return the area code") do
-      test_phone = Phone.new({:number => "555-5555", :area_code => "503"})
+      test_phone = Phone.new({:number => "555-5555", :area_code => "503", :label => "mobile"})
       expect(test_phone.area_code()).to(eq("503"))
+    end
+  end
+
+  describe("#label") do
+    it("returns the label for the number") do
+      test_phone = Phone.new({:number => "555-5555", :area_code => "503", :label => "mobile"})
+      expect(test_phone.label()).to(eq("mobile"))
     end
   end
